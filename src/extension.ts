35765eeeb2816af6defc74c27f217e1c22bb42c8
Mokhtar Moithu <mokhtar.moithu@delaware.pro>
2023-03-08 11:08:50 +0800

    remove other line and keep remaining line next to each other

--- src/extension.ts
+++ src/extension.ts
@@ -21,17 +21,17 @@ export function activate(context: vscode.ExtensionContext) {
     // Get the document and make edits to it
     const document = editor.document;
     const edit = new vscode.WorkspaceEdit();
+    let concatenatedLines = '';
     for (let i = 0; i < document.lineCount; i++) {
       const line = document.lineAt(i);
       if (line.text.includes(text)) {
-        // Keep this line
-        continue;
-      } else {
-        // Remove this line
-        const range = new vscode.Range(line.range.start, line.range.end);
-        edit.delete(document.uri, range);
+        // Keep this line and concatenate it with the previous kept line
+        concatenatedLines += line.text.trim() + '\n';
       }
     }
+    // Replace the entire document with the concatenated lines
+    const range = new vscode.Range(document.positionAt(0), document.positionAt(document.getText().length));
+    edit.replace(document.uri, range, concatenatedLines);
     await vscode.workspace.applyEdit(edit);
   });
 
