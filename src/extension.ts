c01f5a114018f765114a41d781e075507126c23d
Mokhtar <mokh223@gmail.com>
2023-03-31 20:45:47 +0800

    multi keyword split by comma

--- src/extension.ts
+++ src/extension.ts
@@ -11,12 +11,16 @@ export function activate(context: vscode.ExtensionContext) {
 
     // Prompt the user for text
     const text = await vscode.window.showInputBox({
-      prompt: 'Enter the text to keep',
+      prompt: 'Enter the text to keep, separated by semicolons (;)',
+      value: 'CODE_UNIT_STARTED;CODE_UNIT_FINISHED;USER_DEBUG'
     });
 
     if (!text) {
       return;
     }
+
+    // Split the input into keywords
+    const keywords = text.split(';');
 
     // Get the document and make edits to it
     const document = editor.document;
@@ -24,9 +28,13 @@ export function activate(context: vscode.ExtensionContext) {
     let concatenatedLines = '';
     for (let i = 0; i < document.lineCount; i++) {
       const line = document.lineAt(i);
-      if (line.text.includes(text)) {
-        // Keep this line and concatenate it with the previous kept line
-        concatenatedLines += line.text.trim() + '\n';
+      for (let j = 0; j < keywords.length; j++) {
+        const keyword = keywords[j];
+        if (line.text.includes(keyword)) {
+          // Keep this line and concatenate it with the previous kept line
+          concatenatedLines += line.text.trim() + '\n';
+          break;
+        }
       }
     }
     // Replace the entire document with the concatenated lines
